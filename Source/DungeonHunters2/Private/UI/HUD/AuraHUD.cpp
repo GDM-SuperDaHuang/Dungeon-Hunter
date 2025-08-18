e8159c21f23bfa7e347b71afe079e5557fa95d35
gdm <1875525594@qq.com>
2025-08-18 23:42:52 +0800

    2

--- Source/DungeonHunters2/Private/UI/HUD/AuraHUD.cpp
+++ Source/DungeonHunters2/Private/UI/HUD/AuraHUD.cpp
@@ -13,6 +13,8 @@ UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetCont
 	{
 		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
 		OverlayWidgetController->SetWidgetControllerParams(WCParams);
+		OverlayWidgetController->BindCallbacksToDependences();
+		
 		return OverlayWidgetController;
 	}
 	return OverlayWidgetController;
