4ef51819f591fdb17689734d8ea5d97be2061fd9
be_gdm <MengYen333@outlook.com>
2025-12-18 20:28:08 +0800

    1

--- Source/DungeonHunters2/Private/AuraAbilityTypes.cpp
+++ Source/DungeonHunters2/Private/AuraAbilityTypes.cpp
@@ -2,9 +2,9 @@
 
 bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
 {
-	// uint32 b
+	// uint32 b 消息头
 	uint32 RepBits = 0;
-	if (Ar.IsSaving())
+	if (Ar.IsSaving())// 如果是写包行为，内存 → Archive → 网络
 	{
 		if (bReplicateInstigator && Instigator.IsValid())
 		{
@@ -72,11 +72,14 @@ bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* M
 			RepBits |= 1 << 15;
 		}
 	}
-	Ar.SerializeBits(&RepBits, 15);
 
-	if (RepBits & (1 << 0))
+	// 从 Ar 读取15个bit到RepBits，
+	// 15，有问题？只序列化 bit 0 ~ bit 14，读不到 KnockbackForce？
+	Ar.SerializeBits(&RepBits, 16);
+
+	if (RepBits & (1 << 0)) //对应的位置如果有效，则写入值
 	{
-		Ar << Instigator;
+		Ar << Instigator;// 如果是Saving，则是把 Instigator 写入Ar；如果是Loading，则是把从 Ar 读出数据，赋给 Instigator
 	}
 	if (RepBits & (1 << 1))
 	{
@@ -96,13 +99,19 @@ bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* M
 	}
 	if (RepBits & (1 << 5))
 	{
-		if (Ar.IsLoading())
+		if (Ar.IsLoading())//如果是读包
 		{
 			if (!HitResult.IsValid())
 			{
+				//FHitResult是TSharedPtr类型的指针，不是 UObject子类，没有 NetGUID，需要解引用,赋默认值，
+				// 防止HitResult为null，奔溃
 				HitResult = TSharedPtr<FHitResult>(new FHitResult());
 			}
 		}
+		/**
+		 * 如果一个类型没有实现 << 操作符重载，而只有 NetSerialize，就必须调用 NetSerialize。
+		 * 反之，如果类型实现了 <<，用 NetSerialize 可能会编译错误或无效。
+		*/
 		HitResult->NetSerialize(Ar, Map, bOutSuccess);
 	}
 	if (RepBits & (1 << 6))
@@ -148,9 +157,12 @@ bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* M
 		{
 			if (!DamageType.IsValid())
 			{
+				//解引用，写默认值
 				DamageType = TSharedPtr<FGameplayTag>(new FGameplayTag());
 			}
 		}
+		// IsLoading时，将Ar内容写入DamageType，
+		// IsSaving 时，将DamageType被人写入Ar
 		DamageType->NetSerialize(Ar, Map, bOutSuccess);
 	}
 
@@ -165,9 +177,9 @@ bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* M
 	}
 	
 
-
 	if (Ar.IsLoading())
 	{
+		// 更新施加者的ASC
 		AddInstigator(Instigator.Get(), EffectCauser.Get()); // Just to initialize InstigatorAbilitySystemComponent
 	}
 
